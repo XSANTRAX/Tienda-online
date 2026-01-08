145fc3b6a60140829a9e1f9654523108046a76ab
Santiago Poveda <santypoveda1112@gmail.com>
2026-01-08 15:10:14 -0500

    Creación del componente Footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+import styles from './Footer.module.css'
+
+export function Footer () {
+  return (
+    <footer className={styles.info}>
+      <small>&copy; 2025 DevJobs. Todos los derechos reservados.</small>
+    </footer>
+  )
+}
