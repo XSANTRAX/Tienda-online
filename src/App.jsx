144484ad75ee8221642666e6fbc3fa1f5e64c3ef
Santiago Poveda <santypoveda1112@gmail.com>
2026-01-08 15:09:28 -0500

    Cambios en el archivo App

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Header } from "./Components/Header.jsx";
+import { Footer } from "./Components/Footer.jsx";
+import { Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
 
+const Home = lazy(() => import("./Pages/home.jsx"));
+const Details = lazy(() => import("./Pages/Details.jsx"));
+const NotFoundPage = lazy(() => import("./Pages/404.jsx"));
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Suspense
+        fallback={
+          <div
+            style={{ maxWidth: "1280px", margin: "0 auto", padding: "0 1rem" }}
+          >
+            Cargando...
+          </div>
+        }
+      >
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<Details />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+        <Footer />
+      </Suspense>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
