946ffcb7ae8447178724f486fb998e7d8890d772
Santiago Poveda <santypoveda1112@gmail.com>
2026-01-13 10:41:19 -0500

    Cambios en el componente de Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.css'
 export function Footer () {
   return (
     <footer className={styles.info}>
-      <small>&copy; 2025 DevJobs. Todos los derechos reservados.</small>
+      <small>&copy; 2025 Angels. Todos los derechos reservados.</small>
     </footer>
   )
 }
